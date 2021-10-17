42cc12a842c7cd266df6d96f3cc73e8aa69fa11e
FST0524 <fabi.stricker@gmx.de>
2021-10-17 19:58:46 +0200

    "Added Swagger UI, query for distinct fields"

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,31 @@ from sys import platform
 from flask import Flask
 from elasticsearch import Elasticsearch
 from flask.wrappers import Request
+from flask_swagger_ui import get_swaggerui_blueprint
 
 es_client = Elasticsearch("http://node-2.hska.io:9200")
 app = Flask(__name__)
+
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Video Games Sales Search Engine"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+
+def get_field_distinct(field):
+    query = {
+        "unique_field": {
+            "terms": {
+                "field": field
+            }
+        }
+    }
+    return query
 
 def buildQuery():
     query = {
@@ -19,19 +41,25 @@ def index():
     result = es_client.info()
     return result
     
-@app.route('/getPlatformTags', methods=['GET'])
+@app.route('/api/getPlatformTags', methods=['GET'])
 def getPlatTags():
-    return "PlatformTags"
+    dist_query = get_field_distinct("Platform")
+    result = es_client.search(index="vgsales",aggs=dist_query,size=0)
+    return result
 
-@app.route('/getGenreTags', methods=['GET'])
+@app.route('/api/getGenreTags', methods=['GET'])
 def getGenreTags():
-    return "GenreTags"
+    dist_query = get_field_distinct("Genre")
+    result = es_client.search(index="vgsales",aggs=dist_query,size=0)
+    return result
 
-@app.route('/getPublisherTags', methods=['GET'])
+@app.route('/api/getPublisherTags', methods=['GET'])
 def getPublisherTags():
-    return "PublisherTags"
+    dist_query = get_field_distinct("Publisher")
+    result = es_client.search(index="vgsales",aggs=dist_query,size=0)
+    return result
 
-@app.route('/search', methods=['GET'])
+@app.route('/api/search', methods=['GET'])
 def getSearchRequest():
     
     # rank = Request.form['Rank']
